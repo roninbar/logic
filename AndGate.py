1144aaa7ecf488158810cf27eeb5920703cdea1e
ron <rinbar70@gmail.com>
2022-01-12 23:51:16 +0200

    Fix initial value for `AndGate`.

--- AndGate.py
+++ AndGate.py
@@ -8,5 +8,5 @@ from MultiGate import MultiGate
 class AndGate(MultiGate):
 
     def __init__(self, inputs: Iterable[Gate]):
-        super(AndGate, self).__init__(inputs, operator.and_, False, 'and')
+        super(AndGate, self).__init__(inputs, operator.and_, True, 'and')
 
